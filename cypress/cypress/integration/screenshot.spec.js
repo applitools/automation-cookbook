f82858478b157228211d77054360ad3c05f5326f
Colby Fayock <fayock@gmail.com>
2021-03-29 23:31:00 -0400

    removing fail test

--- cypress/cypress/integration/screenshot.spec.js
+++ cypress/cypress/integration/screenshot.spec.js
@@ -10,12 +10,4 @@ describe("Screenshot", () => {
     cy.get('#photo-upload').attachFile('images/french-toast.jpg');
     cy.get('form figure').screenshot();
   });
-  
-  it('should take a screenshot after a failed assertion', () => {
-    cy.on('error', (error, runnable) => {
-      cy.get('form').screenshot();
-      throw error // throw error to have test still fail
-    })
-    cy.get('form figure').screenshot();
-  });
 });
