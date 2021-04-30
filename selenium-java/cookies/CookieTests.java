93acb48e2cd90e2280e72fbb2d15b6b93f2e3de4
Angie Jones <jones.angie@gmail.com>
2021-04-29 23:28:01 -0500

    Update CookieTests.java

--- selenium-java/cookies/CookieTests.java
+++ selenium-java/cookies/CookieTests.java
@@ -1,7 +1,6 @@
 package cookies;
 
 import base.BaseTests;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
