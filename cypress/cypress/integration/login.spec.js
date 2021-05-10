a50652cb03e945cf01ef9cb9211fd28c41fc8ff7
Angie Jones <jones.angie@gmail.com>
2021-05-09 21:27:09 -0500

    Update login.spec.js

--- cypress/cypress/integration/login.spec.js
+++ cypress/cypress/integration/login.spec.js
@@ -1,10 +1,7 @@
 describe("Login", () => {
 
-  beforeEach(() => {
-    cy.visit('https://demo.applitools.com/');
-  });
-
   it('should log into the application', () => {
+    cy.visit('https://demo.applitools.com/');
     cy.get('#username').type('colbyfayock');
     cy.get('#password').type('Password1234');
     cy.get('#log-in').click();
