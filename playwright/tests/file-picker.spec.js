e7bfc89624d6f32a7984328ae1d299e889de67a5
colbyfayock <fayock@gmail.com>
2021-08-11 15:38:56 -0400

    adding file upload / picker

---
+++ playwright/tests/file-picker.spec.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe("Screenshot", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://kitchen.applitools.com/ingredients/file-picker');
+  });
+
+  test('should upload a photo to the file picker', async ({ page }) => {
+    await page.setInputFiles('#photo-upload', './fixtures/images/french-toast.jpg');
+    const $photoUpload = await page.$('#photo-upload');
+    const $photoUploadParent = await $photoUpload.$('xpath=..');
+    const $photoUploadFigure = await $photoUploadParent.$$('figure');
+    expect($photoUploadFigure).toHaveLength(1);
+  });
+});
